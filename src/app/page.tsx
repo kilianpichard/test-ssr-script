7ffffe7132577ec5619ec6c5d7f8840e91084cdd
Kilian Pichard <perso@kilianp.fr>
2025-02-14 16:42:07 +0100

    hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home !</h1>
       <Link href="/widget">Aller au widget !</Link>
     </div>
   );
